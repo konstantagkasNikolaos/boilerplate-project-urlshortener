b555c756766801c16c321b178036d97f932f1084
konstantagkas <konstantagkas.nikolaos@gmail.com>
2022-10-14 15:31:30 +0300

    fix www

--- tools.js
+++ tools.js
@@ -3,12 +3,12 @@ const dns = require("dns");
 const checkUrl = (url) => {
   let valid = true;
 
-  const urlStart = /^(https|http):\/\/www./g;
+  const urlStart = /^(https|http):\/\//g;
   const start = url.match(urlStart);
 
   if (!start) return false;
 
-  const urlPart = url.replace(start, "");
+  const urlPart = url.replace(start, "").replace("www.", "");
 
   dns.lookup(urlPart, {}, (err, address, family) => {
     if (err) valid = false;
